75b2912a2dd7a9d8cc822f80affd69cfc6064178
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2025-01-12 20:12:51 +0700

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,7 @@
 <div class="static bottom-0 mx-auto bg-primary mt-10 text-white">
     <div class="mx-auto border-b-white border-y">
         <div class="container mx-auto">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-5 p-5">
-                <!-- Bagian Logo -->
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-5 p-5">            
                 <div class="col-span-1 md:col-span-1 lg:col-span-4">
                     <div class="flex gap-3 mb-6 justify-center md:justify-start">
                         <img class="w-1/6 lg:w-1/4" src="{{ asset('assets/logo_kmtembus.png') }}" alt="Logo KM TEMBUS">
@@ -19,7 +18,6 @@
                     </div>
                 </div>
 
-                <!-- Bagian Menu -->
                 <div class="col-span-1 md:col-span-1 lg:col-span-3">
                     <ul class="list-none space-y-2 lg:space-y-4 text-center lg:text-left">
                         <li>
@@ -50,7 +48,6 @@
                     </ul>
                 </div>
 
-                <!-- Bagian Maps -->
                 <div class="col-span-1 md:col-span-2 lg:col-span-5">
                     <iframe class="w-full h-48 md:h-60 lg:h-72"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3474.935992122658!2d110.43872833916379!3d-7.059588774802057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708f2f3a71bdff%3A0xdf569bd396b1179!2sBasecamp%20Keluarga%20Mahasiswa%20Tembalang%20Kudus!5e1!3m2!1sid!2sid!4v1735748992879!5m2!1sid!2sid"
